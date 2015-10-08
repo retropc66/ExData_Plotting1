9c8953990056fe1ac1845748e4aed758709f9cf8
Christopher Porter <retropc@gmail.com>
2015-10-08 12:47:12 -0400

    Updates to axis labels, and legend for plot4

--- plot3.R
+++ plot3.R
@@ -9,7 +9,7 @@ data<-data[data$datetime>="2007-02-01" & data$datetime<"2007-02-03" & !is.na(dat
 
 # Plot figure: N.B. The default png size (480x480) matches the required size
 png("plot3.png")
-with(data,plot(datetime,Sub_metering_1,type="l"),xlab="",ylab="Energy sub metering")
+with(data,plot(datetime,Sub_metering_1,type="l",xlab="",ylab="Energy sub metering"))
 with(data,lines(datetime,Sub_metering_2,type="l",col="red"))
 with(data,lines(datetime,Sub_metering_3,type="l",col="blue"))
 legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
