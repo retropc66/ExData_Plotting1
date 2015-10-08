d16701f98d8dd6d74607bfef2181f6efbc83ca06
Christopher Porter <retropc@gmail.com>
2015-10-08 12:04:49 -0400

    First draft of the scripts and figures - looks largely correct, but worth checking before submitting the repo.  Not adding data (also in this dir) to the repo!

---
+++ plot2.R
@@ -0,0 +1,13 @@
+# Read data
+data<-read.table("household_power_consumption.txt",sep=";",header=T,na.string="?")
+
+# Combine and convert date and time columns (factors) into a single POSIXlt date/time field
+data$datetime=strptime(paste(as.character(data$Date),as.character(data$Time)),format="%d/%m/%Y %H:%M:%S")
+
+# Subset the two days of interest, replacing the original data object
+data<-data[data$datetime>="2007-02-01" & data$datetime<"2007-02-03" & !is.na(data$datetime),]
+
+# Plot figure: N.B. The default png size (480x480) matches the required size
+png("plot2.png")
+with(data,plot(datetime,Global_active_power,type="l",xlab="",ylab="Global Active Power (kilowatts)"))
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,16 @@
+# Read data
+data<-read.table("household_power_consumption.txt",sep=";",header=T,na.string="?")
+
+# Combine and convert date and time columns (factors) into a single POSIXlt date/time field
+data$datetime=strptime(paste(as.character(data$Date),as.character(data$Time)),format="%d/%m/%Y %H:%M:%S")
+
+# Subset the two days of interest, replacing the original data object
+data<-data[data$datetime>="2007-02-01" & data$datetime<"2007-02-03" & !is.na(data$datetime),]
+
+# Plot figure: N.B. The default png size (480x480) matches the required size
+png("plot3.png")
+with(data,plot(datetime,Sub_metering_1,type="l"),xlab="",ylab="Energy sub metering")
+with(data,lines(datetime,Sub_metering_2,type="l",col="red"))
+with(data,lines(datetime,Sub_metering_3,type="l",col="blue"))
+legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
